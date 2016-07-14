fa9f42b9b5a3a3f972dc7561cb8e6556a3af6a37
rosen-vladimirov <rosen.vladimirov@telerik.com>
2016-07-14 15:29:48 +0300

    Require grunt in prepublish script only if we have to

--- prepublish.js
+++ prepublish.js
@@ -1,10 +1,10 @@
 var fs = require("fs"),
-	path = require("path"),
-	grunt = require("grunt");
+	path = require("path");
 
 var pathToTypeScriptEntryPoint = path.join(__dirname, "lib", "ios-sim.ts");
 if (fs.existsSync(pathToTypeScriptEntryPoint)) {
-	var callback = function(err) {};
+	var callback = function(err) {},
+		grunt = require("grunt");
 
 	grunt.cli.tasks = ["ts:devlib"];
 	grunt.cli(null, callback);
